73de0a5b33caced55d084e731ec3518ab544c084
Kristoffer-Staugaard <kristofferstaugaard@gmail.com>
2026-03-12 14:22:19 +0100

    github actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/portfolio-v3/', // Update this to match your repository name if different
     plugins: [react()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
